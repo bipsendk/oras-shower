49978c9a0af64f16e95a520648a1b7631fa67250
bipsendk <brian.ipsen@ryesgade47c.dk>
2023-01-09 20:41:43 +0100

    Update oras_dsh.h

--- oras_dsh.h
+++ oras_dsh.h
@@ -6,11 +6,24 @@ class DSH {
 
     unsigned long tStartTime;
     unsigned long tLastUpdate;
+    char szLastShowerEnded[32]; // Human readable of tLastUpdate
 
-    float totWaterLiters;
-    float totEnergy;
-    float bathTemp;
-    unsigned long lastTemperature;
+    unsigned long lCurrentShowerNo;  // 
+    unsigned long lLastShowerNo;  // No of previous shower - used to find out if previous shower is being continued
+
+    // float totWaterLiters;
+    // float totEnergy;
+    // float bathTemp;
+
+    float curWaterLiters;
+    float curEnergy;
+    float curBathTemp;
+
+    float lastWaterLiters=0;
+    float lastEnergy=0;
+    float lastBathTemp=0;
+    unsigned long lastShowerDuration=0;
+
     boolean occupancy=0;
 
     uint8_t bleData[20];
